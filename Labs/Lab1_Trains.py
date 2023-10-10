efa040c4045b0f7a4afa104ccfd0d98eb1e5f2c7
Veronica Curry <v.curry20@gmail.com>
2023-10-10 14:47:45 +0100

    lab 1

--- Labs/Lab1_Trains.py
+++ Labs/Lab1_Trains.py
@@ -20,6 +20,9 @@
 
 import requests   # makes a request to a webpage
 import csv        # lists data as a csv / csv format
+import pandas as pd
+import numpy as np
+import re 
 
 from xml.dom.minidom import parseString
 
@@ -35,15 +38,119 @@ doc = parseString(page.content)
 
 # Store the xml in a file. complete the following / can also be commented out
 
-# with open ("trainxml.xml", "w") as xmlfp:
-#    doc.writexml(xmlfp)
+with open ("trainxml.xml", "w") as xmlfp:
+    doc.writexml(xmlfp)
 
+
+# print out the train codes - this prints to console
 objTrainPositionsNodes = doc.getElementsByTagName("objTrainPositions")
 for objTrainPositionsNode in objTrainPositionsNodes:
     trainCodenode = objTrainPositionsNode.getElementsByTagName("TrainCode").item(0)
     trainCode = trainCodenode.firstChild.nodeValue.strip()
     print (trainCode)
     
+
+# print out the longitudes - this prints to console  
+objTrainPositionsNodes = doc.getElementsByTagName("objTrainPositions")
+for objTrainPositionsNode in objTrainPositionsNodes:
+    TrainLongitudenode = objTrainPositionsNode.getElementsByTagName("TrainLongitude").item(0)
+    Longitudecode = TrainLongitudenode.firstChild.nodeValue.strip()
+    print (Longitudecode)
+
+
+# print Train codes to CSV file
+# https://stackoverflow.com/questions/3348460/csv-file-written-with-python-has-blank-lines-between-each-row
+
+with open('traincodes.csv', mode='w', newline='') as train_file:
+    train_writer = csv.writer(train_file, delimiter='\t', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+    objTrainPositionsNodes = doc.getElementsByTagName("objTrainPositions")
+    for objTrainPositionsNode in objTrainPositionsNodes:
+        trainCodenode = objTrainPositionsNode.getElementsByTagName("TrainCode").item(0)
+        trainCode = trainCodenode.firstChild.nodeValue.strip()
+     #print (trainCode)
+
+        dataList = []
+        dataList.append(trainCode)
+        train_writer.writerow(dataList)
+
+# print longitudes to CSV file
+
+with open('Longitudes.csv', mode='w', newline='') as Longitude_file:
+    Longitude_writer = csv.writer(Longitude_file, delimiter='\t', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+    objTrainPositionsNodes = doc.getElementsByTagName("objTrainPositions")
+    for objTrainPositionsNode in objTrainPositionsNodes:
+        TrainLongitudenode = objTrainPositionsNode.getElementsByTagName("TrainLongitude").item(0)
+        Longitudecode = TrainLongitudenode.firstChild.nodeValue.strip()
+    #print (Longitudecode)
+        
+        dataList = []
+        dataList.append(Longitudecode)
+        Longitude_writer.writerow(dataList)
+
+
+# print all details of trains using their tags
+# for loop used
+
+retrieveTags=['TrainStatus',
+'TrainLatitude',
+'TrainLongitude',
+'TrainCode',
+'TrainDate',
+'PublicMessage',
+'Direction'
+]
+
+headername = ['TrainStatus','TrainLatitude','TrainLongitude','TrainCode','TrainDate','PublicMessage','Direction'
+]
+
+with open('train_all.csv', mode='w', newline='') as train_fileall:
+    train_writerall = csv.writer(train_fileall, delimiter='\t', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+
     
-#with open ("traincode.xml", "w") as traincode:
-#        doc.writexml(traincode)
+    dataList2 = []
+    for retrieveTag in retrieveTags:
+        datanode = objTrainPositionsNode.getElementsByTagName(retrieveTag).item(0)
+        dataList2.append(datanode.firstChild.nodeValue.strip())
+
+        train_writerall.writerow(dataList2)
+
+
+# print all details of trains using their tags
+# for loop used
+
+retrieveTags=['TrainStatus',
+'TrainLatitude',
+'TrainLongitude',
+'TrainCode',
+'TrainDate',
+'PublicMessage',
+'Direction'
+]
+
+
+with open('train_D.csv', mode='w', newline='') as train_fileall:
+    train_writerall = csv.writer(train_fileall, delimiter='\t', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+   
+    dataList2 = []
+    for retrieveTag in retrieveTags:
+        datanode = objTrainPositionsNode.getElementsByTagName(retrieveTag).item(0)
+        dataList2.append(datanode.firstChild.nodeValue.strip())
+
+        train_writerall.writerow(dataList2)
+
+# https://www.quora.com/How-can-you-filter-large-CSV-files-in-Python
+# https://www.geeksforgeeks.org/python-find-the-list-elements-starting-with-specific-letter/
+
+check = "D"
+
+with open('train_D.csv', 'r') as input_file, open('output_file.csv', 'w', newline='') as output_file: 
+    reader = csv.reader(input_file) 
+    writer = csv.writer(output_file) 
+
+    for row in reader: 
+        if row[0] == re.match("^{}".format(check), trainCode.lower()): 
+            writer.writerow(row) 
+
