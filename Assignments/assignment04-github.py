b53b6500b5b5e6750cda2047072f42e18ede3dc1
Veronica Curry <v.curry20@gmail.com>
2023-10-24 16:52:32 +0100

    assignment 4

--- Assignments/assignment04-github.py
+++ Assignments/assignment04-github.py
@@ -6,38 +6,43 @@
 
 from github import Github
 import requests
+import json
 from config import config as cfg
 
 import numpy as np
 import pandas as pd
 
+url = "https://api.github.com/repos/VCurry20/data-representation-coursework"
+#datatest = pd.read_csv("employees.csv")
+
+# reading the CSV file 
+text = open("employees.csv", "r") 
+  
+#join() method combines all contents of  
+# csvfile.csv and formed as a string 
+text = ''.join([i for i in text])  
+  
+# search and replace the contents 
+text = text.replace("Andrew", "Veronica")  
+
+  
+# output.csv is the output file opened in write mode 
+x = open("output.csv","w") 
+  
+# all the replaced text is written in the output.csv file 
+x.writelines(text) 
+x.close()
 
 
 
+#def replace_char(string):
+#    new_str = ""
+#    for c in string:
+#        if c ==  "Andrew":
+#            new_str += "Veronica"
+#       else:
+#            new_str += c
+#    return new_str
 
 
-
-
-
-
-
-
-
-
-
-
-
-# Question 3
-# Write a function which takes in a string and replaces all occurrences of 'e' with '*' and returns the resulting string
-
-def replace_char(string):
-    new_str = ""
-    for c in string:
-        if c ==  "e":
-            new_str += "*"
-        else:
-            new_str += c
-    return new_str
-
-
-print(replace_char("veronica"))
+#print(replace_char(datatest))
