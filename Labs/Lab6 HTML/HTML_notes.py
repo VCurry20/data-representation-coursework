b4bdf3f394bd442118bf7e62707b8004d154750d
Veronica Curry <v.curry20@gmail.com>
2023-10-25 22:16:19 +0100

    tip 6 lectures

---
+++ Labs/Lab6 HTML/HTML_notes.py
@@ -0,0 +1,19 @@
+# Walk through of topic 6 - HTML
+
+# to open webpages - right click on the files from the list and reveal in file explorer
+
+# https://www.w3schools.com/html/
+# With HTML we will be making an interface to interact with an API
+# Jquery and Ajax 
+
+
+# useful sites
+# https://getbootstrap.com/
+# add code to heading and then can quickly update your css to make the page look better
+# this code was added on the top and it actions the "class= "table" " tag
+
+
+## Javascript
+# notes in addition to slides
+# The place where you set style closest to the element will be the one it goes with
+# for example - set text colour as green in the head, but in the element itself you set it as red - the text will be red
